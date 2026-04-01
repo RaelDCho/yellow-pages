0dff35fca456d66070d3abd96e417ad150dc93b6
David Cho <davidcho@Davids-MacBook-Air.local>
2026-04-02 01:37:16 +1100

    Added DB capabilities

--- back-end/mongo.js
+++ back-end/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// quit immediately if args !== 5 or 3
+// quit immediately if args !== 5 and args !== 3
 if (process.argv.length !== 5 && process.argv.length !== 3) {
     console.log('Error');
     process.exit(1);
@@ -13,39 +13,40 @@ const url = `mongodb+srv://davidcho123456781_db_user:${password}@cluster0.nvtf0j
 mongoose.set('strictQuery', false);
 
 // family: 4 = IPv4 connection - mongoose only supports IPv4
-mongoose.connect(url, {family: 4});
+mongoose.connect(url, {family: 4}).then(result => {
+    console.log('Connection success');
+});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+});
+
+const Person = mongoose.model('Person', personSchema);
 
 // if 5 arguments we are adding a person
 if (process.argv.length === 5) {
     console.log(5);
-    // const personSchema = new mongoose.Schema({
-    //     name: String,
-    //     number: String
-    // });
+    
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4].toString()
+    });
 
-    // const Person = mongoose.model('Person', personSchema);
-
-    // const person = new Person({
-    //     name: 'Inaho Kaizaku',
-    //     number: '12-12-1231451'
-    // });
-
-    // person.save().then(result => {
-    //     console.log('saved!');
-    //     mongoose.connection.close();
-    // });
+    person.save().then(result => {
+        console.log(`${person.name} (${person.number}) has been saved in the phonebook.`);
+        mongoose.connection.close();
+    });
 } else if (process.argv.length === 3) {
     console.log(3);
     // output all in the database
-    // Person.find({}).then(result => {
-    //     result.forEach(person => {
-    //         console.log(person);
-    //         mongoose.connection.close();
-    //     });
-    // });
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(`${person.name} | ${person.number}`);
+            mongoose.connection.close();
+        });
+    });
 } else {
     console.log('Error');
     process.exit(1);
 }
-
-mongoose.connection.close();
