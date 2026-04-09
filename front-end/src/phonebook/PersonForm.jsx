1aeabe236f0b6c692bd035bd5f80536afcc7303b
David Cho <davidcho@Davids-MacBook-Air.local>
2026-04-09 12:10:22 +1000

    Adjusted field for adding new contact

--- front-end/src/phonebook/PersonForm.jsx
+++ front-end/src/phonebook/PersonForm.jsx
@@ -5,12 +5,8 @@ const PersonForm = ({ newName, newNumber, addNewPerson, handleNameChange, handle
       <h3>Add New</h3>
       <form onSubmit={addNewPerson} className='form'>
         <div className='bar'>
-          <div className='contact-input'>
-            <input value={newName} placeholder='name' onChange={handleNameChange} className='input'/>
-          </div>
-          <div className='contact-input'>
-            <input value={newNumber} placeholder='number' onChange={handleNumberChange} className='input'/>
-          </div>
+          <input value={newName} placeholder='name' onChange={handleNameChange} className='input'/>
+          <input value={newNumber} placeholder='number' onChange={handleNumberChange} className='input'/>
           <div className='submit-button'>
             <button type="submit">+</button>
           </div>
